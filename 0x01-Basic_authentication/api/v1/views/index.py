5cdd622b1cddb9c446ecb89701ee1e3d34f08486
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-05-21 22:36:46 +0000

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
-def unauthorized():
-    """Endpoint that raises a 401 error"""
+@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+def get_unauthorized():
+    """Endpoint to trigger a 401 error"""
     abort(401)
