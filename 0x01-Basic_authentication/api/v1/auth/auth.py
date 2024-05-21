d1ad306ec7ab3b593bcb47ae51b178e9b945ded8
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-05-21 23:33:54 +0000

    Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -29,8 +29,12 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
-        """Returns None as placeholder"""
-        return None
+        """Returns the value of the Authorization header"""
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns None as placeholder"""
