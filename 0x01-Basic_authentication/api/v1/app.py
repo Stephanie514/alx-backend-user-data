80122ad5bf261072cbd5cc34ca907086138a16e3
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-05-22 20:14:46 +0000

    Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,10 +6,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-<<<<<<< HEAD
-=======
-import os
->>>>>>> c48a62adbbb9d99f39163e17902a8a0efecfeb67
 from api.v1.auth.auth import Auth
 
 app = Flask(__name__)
@@ -20,10 +16,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
 
-if AUTH_TYPE == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-elif AUTH_TYPE == "auth":
+if AUTH_TYPE == "auth":
     auth = Auth()
 
 
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-""" This is a BasicAuth class module """
-from api.v1.auth.auth import Auth
-
-class BasicAuth(Auth):
-    """ This class that inherits from Auth """
-    pass
