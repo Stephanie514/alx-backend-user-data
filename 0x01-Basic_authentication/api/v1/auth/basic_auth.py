777cc25fea854e00103e026ca232178b7ea6fcbb
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-05-22 19:56:45 +0000

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This is a BasicAuth class module """
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """ This class that inherits from Auth """
+    pass
