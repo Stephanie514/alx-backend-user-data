86f6c491784c0b249e5eda02898c6998315c0de6
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-05-22 21:09:27 +0000

    Basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
+
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """BasicAuth class that inherits from Auth"""
+    """
+    BasicAuth class for managing basic authentication.
+    Usage:
+        # Imports BasicAuth
+        from api.v1.auth.basic_auth import BasicAuth
+
+        # Creates an instance of BasicAuth
+        basic_auth = BasicAuth()
+    """
     pass
