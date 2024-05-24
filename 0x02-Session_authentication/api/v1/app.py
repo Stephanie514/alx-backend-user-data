3ac202906a70ed7dc42e609b32905880194f6451
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-05-24 00:24:05 +0000

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -52,12 +52,18 @@ def before_request_handler():
         '/api/v1/status/',
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/'
+        '/api/v1/auth_session/login/'
     ]
+
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
         abort(401)
-    if auth.current_user(request) is None:
+
+    request.current_user = auth.current_user(request)
+    if request.current_user is None:
         abort(403)
 
 
@@ -72,8 +78,6 @@ def before_request():
     if auth.authorization_header(request) is None \
             and auth.session_cookie(request) is None:
         abort(401)
-
-    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
