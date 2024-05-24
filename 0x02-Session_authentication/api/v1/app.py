780a760a811318470149b7610342633ebc5860ac
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-05-24 00:04:37 +0000

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -5,19 +5,16 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
-from flask import Flask, jsonify, request
 from models.user import User
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-auth = Auth()
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-# Initializing the auth variable
 auth = None
 AUTH_TYPE = getenv("AUTH_TYPE")
 
@@ -66,6 +63,15 @@ def before_request_handler():
 @app.before_request
 def before_request():
     """Runs before each request"""
+    excluded_paths = ['/api/v1/auth_session/login/']
+
+    if request.path in excluded_paths:
+        return None
+
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
+        abort(401)
+
     request.current_user = auth.current_user(request)
 
 
