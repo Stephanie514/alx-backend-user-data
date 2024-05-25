033c9263eec4e256eb0f55cb1fa6ffc59901f722
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-05-25 22:57:17 +0000

    Expiration?

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -51,33 +51,17 @@ def before_request_handler():
     excluded_paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
         '/api/v1/auth_session/login/'
     ]
-
     if not auth.require_auth(request.path, excluded_paths):
         return
-
-    if (auth.authorization_header(request) is None and
-            auth.session_cookie(request) is None):
-        abort(401)
-
+    if auth.authorization_header(request) is None:
+        if auth.session_cookie(request) is None:
+            abort(401)
     request.current_user = auth.current_user(request)
     if request.current_user is None:
         abort(403)
-
-
-@app.before_request
-def before_request():
-    """Runs before each request"""
-    excluded_paths = ['/api/v1/auth_session/login/']
-
-    if request.path in excluded_paths:
-        return None
-
-    if auth.authorization_header(request) is None \
-            and auth.session_cookie(request) is None:
-        abort(401)
 
 
 if __name__ == "__main__":
