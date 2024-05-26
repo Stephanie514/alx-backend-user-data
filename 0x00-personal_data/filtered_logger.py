23e955f7ccfb085dc847b1f4b26c9bf8f286182c
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-05-26 12:31:04 +0000

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -66,8 +66,8 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
 
 def main():
     """
-    Obtain a database connection using get_db and retrieve all rows
-    in the users table and display each row under a filtered format
+    Gets database connection using get_db; retrieves all rows
+    users' table. Displays each row under a filtered format
     """
     db = get_db()
     cursor = db.cursor()
