98a47d22924b238dbaf713f454d83af1014e7a58
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-05-30 19:33:48 +0000

    End-to-end integration test

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,10 +1,95 @@
 #!/usr/bin/env python3
 """
-Main file
+Main.py file
 """
-from user import User
+import requests
 
-print(User.__tablename__)
+# Define the base URL for the Flask app
+BASE_URL = 'http://127.0.0.1:5000'
 
-for column in User.__table__.columns:
-    print("{}: {}".format(column, column.type))
+
+def register_user(email: str, password: str) -> None:
+    """ Register a new user and assert the response """
+    url = f"{BASE_URL}/users"
+    data = {'email': email, 'password': password}
+    response = requests.post(url, data=data)
+    assert response.status_code == 200
+    assert response.json() == {"email": email, "message": "user created"}
+
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """ Attempt to log in with the wrong password and assert the response """
+    url = f"{BASE_URL}/sessions"
+    data = {'email': email, 'password': password}
+    response = requests.post(url, data=data)
+    assert response.status_code == 401
+
+
+def log_in(email: str, password: str) -> str:
+    """ Log in with the correct password and return the session ID """
+    url = f"{BASE_URL}/sessions"
+    data = {'email': email, 'password': password}
+    response = requests.post(url, data=data)
+    assert response.status_code == 200
+    assert "session_id" in response.cookies
+    return response.cookies["session_id"]
+
+
+def profile_unlogged() -> None:
+    """ Attempt to access the profile without logging in
+        and assert the response """
+    url = f"{BASE_URL}/profile"
+    response = requests.get(url)
+    assert response.status_code == 403
+
+
+def profile_logged(session_id: str) -> None:
+    """ Access the profile with a valid session ID and assert the response """
+    url = f"{BASE_URL}/profile"
+    cookies = {'session_id': session_id}
+    response = requests.get(url, cookies=cookies)
+    assert response.status_code == 200
+
+
+def log_out(session_id: str) -> None:
+    """ Log out the user and assert the response """
+    url = f"{BASE_URL}/sessions"
+    cookies = {'session_id': session_id}
+    response = requests.delete(url, cookies=cookies)
+    assert response.status_code == 200
+
+
+def reset_password_token(email: str) -> str:
+    """ Requests a password reset token and return it """
+    url = f"{BASE_URL}/reset_password"
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    assert response.status_code == 200
+    return response.json()["reset_token"]
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """ Updating the user's password and assert the response """
+    url = f"{BASE_URL}/reset_password"
+    data = {'email': email, 'reset_token': reset_token, 'new_password': new_password}
+    response = requests.put(url, data=data)
+    assert response.status_code == 200
+    assert response.json() == {"email": email, "message": "Password updated"}
+
+
+# Defines the test credentials
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
